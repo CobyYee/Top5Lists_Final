aaa75a6601a1d689ab75665b102cfbcbe2b0bb94
CobyYee <coby610@gmail.com>
2021-11-03 21:21:17 -0400

    Fixed bug for unmarking deletion list

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -29,7 +29,7 @@ export default function BasicModal() {
   }
 
   const cancelDeletion = () => {
-      store.unmarkListMarkedForDeletion();
+      store.unmarkListForDeletion();
   }
 
   const handleDeletion = () => {
